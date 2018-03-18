679aa2ff17ad25798093f7dd5addf3149e334ffe
Larry Madeo <lmadeo76@gmail.com>
2018-03-18 04:35:05 +0000

    added psuedocode

--- bin/RPi-side/do_something.py
+++ bin/RPi-side/do_something.py
@@ -6,7 +6,7 @@ an http request hitting the get-server program'''
 import datetime
 from file_access import reversed_lines
 
-fname = "time_stamps.txt"
+# fname = "time_stamps.txt"
 
 
 def append_timestamp(fname):
@@ -14,8 +14,17 @@ def append_timestamp(fname):
     '''
     json_time = datetime.datetime.now()
     with open(fname, mode='a+', encoding='utf-8') as f:
-
-        f.write(str(json_time) + '\n')
+        # go to prev last line
+        prev_time = next(islice(reversed_lines(f), 1))
+        # subtract prev_time from now, to the nearest minute
+        # deal with rounding, import relative time?
+        # every minute that is missing is a minute where no connectivity
+        # was detected.
+        # for minute_offset in range(minutes-missed):
+        #     # create crappy_time = timestamp - minute_offset
+        #      # f.write('[crappy]' + str(crappy_time) + '\n')
+        # Question: does with-context carry to the subroutine above?
+        f.write('[good]:' + str(json_time) + '\n')
 
 
 if __name__ == "__main__":
