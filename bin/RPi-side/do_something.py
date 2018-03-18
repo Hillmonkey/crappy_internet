3f1d9d04f463a7d2185ea0e1b5d9fb9817217510
Larry Madeo <lmadeo76@gmail.com>
2018-03-18 01:42:42 +0000

    pep8 :)

--- bin/RPi-side/do_something.py
+++ bin/RPi-side/do_something.py
@@ -4,12 +4,19 @@
 an http request hitting the get-server program'''
 
 import datetime
+from file_access import reversed_lines
 
-file_path = "time_stamps.txt"
+fname = "time_stamps.txt"
+
 
 def append_timestamp(fname):
     '''append timestamp to a file full of timestamps!
     '''
     json_time = datetime.datetime.now()
-    with open(fname, mode='a', encoding='utf-8') as f:
+    with open(fname, mode='a+', encoding='utf-8') as f:
+
         f.write(str(json_time) + '\n')
+
+
+if __name__ == "__main__":
+    pass
