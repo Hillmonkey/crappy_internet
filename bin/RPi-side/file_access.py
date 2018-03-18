9a6f87df1b36b32a5ec7e03a5593d3a77d01df31
Larry Madeo <lmadeo76@gmail.com>
2018-03-18 01:40:40 +0000

    first commit, file_access.py

---
+++ bin/RPi-side/file_access.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+'''contains file access functionality not directly available from Python
+modules
+'''
+
+import os
+from itertools import islice
+
+
+def reversed_lines(file):
+    '''Generate the lines of file in reverse order.'''
+    part = ''
+    for block in reversed_blocks(file):
+        for c in reversed(block):
+            if c == '\n' and part:
+                yield part[::-1]
+                part = ''
+            part += c
+    if part:
+        yield part[::-1]
+
+
+def reversed_blocks(file, blocksize=4096):
+    '''Generate blocks of file's contents in reverse order.'''
+    file.seek(0, os.SEEK_END)
+    here = file.tell()
+    while 0 < here:
+        delta = min(blocksize, here)
+        here -= delta
+        file.seek(here, os.SEEK_SET)
+        yield file.read(delta)
+
+
+def check_last_10_lines(file, key):
+    for line in islice(reversed_lines(file), 10):
+        if line.rstrip('\n') == key:
+            print('FOUND')
+            break
+
+if __name__ == "__main__":
+
+    filename = 'post_server.py'
+    file = open(filename, 'r')
+    for line in islice(reversed_lines(file), 10):
+        print(line.rstrip('\n'))
+
+    file.close()
